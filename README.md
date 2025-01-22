dad22621c403f531ad060a271733f18e66baf34b
Jason Yu <hangyu@paypal.com>
2025-01-22 17:35:04 +0800

    Update script and readme.

--- README.md
+++ README.md
@@ -1,21 +1,29 @@
-# social-media-follower-widget
-A Scriptable script for the IOS widget that displays the followers on **Bilibili** and **YouTube**.
+## social-media-follower-widget
+
+<img align="right" width="200" src="https://github.com/efficlab/social-media-follower-widget/blob/main/imgs/example.jpeg?raw=true">
+
+A Scriptable script for adding IOS widget that displays count of followers on **Bilibili** and **YouTube**.
 
 This script requires a Google API Key to fetch the YouTube channel data which needs to be done first by following: https://developers.google.com/youtube/v3/getting-started 
 
-Then modify below fields in the scripts to yours:
+Then modify below fields in the scripts to your own:
+
+### Config Fields
 
 - **NAME**: your channel name.
 - **CHANNEL_AVATAR_URL**: the URL of your channel logo, you need to get this with the Browser inspector.
+
+Below field is required if you have a Bilibili channel (optional):
+
 - **BILI_UID**: your Bilibili channel UID.
+
+Below fields are required if you have a YouTube channel (optional):
+  
 - **YT_API_KEY**: your Google API Key for YouTube.
 - **YT_CHANNEL_ID**: your YouTube channel ID.
 
 Next, load the modified script into **Scriptable** app which is installed on your cell phone.
 
-Finally, you can put the script widget on the home screen and see the result just like below:
-
-<img align="right" width="200" src="https://github.com/efficlab/social-media-follower-widget/blob/main/imgs/example.jpeg?raw=true">
+Finally, you can put the script widget on the home screen and see the result just like above.
 
 ***\*Please note you need to have the network access to YouTube before you can see the data of your YouTube channel.***
-
--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 // You need to replace below config with yours.
 const NAME = 'EfficLab实验室'
 const CHANNEL_AVATAR_URL = 'https://i1.hdslb.com/bfs/face/11d27a1f87f6407970d4dfc20cffe50b6093cabe.jpg@128w_128h_1c_1s.webp'
-const BILI_UID = 5953240
-const YT_API_KEY = '[Google API Key]'
-const YT_CHANNEL_ID = 'UCpN3prDRM6G-3MWMZvo_jXQ'
+const CHANNEL_CONFIGS = {
+  BILI_UID: 5953240,
+  YT_API_KEY: '[Googla API Key]',
+  YT_CHANNEL_ID: 'UCpN3prDRM6G-3MWMZvo_jXQ'
+}
 
 // Other internal configs.
-const BILI_REQ_URL = `https://api.bilibili.com/x/relation/stat?vmid=${BILI_UID}`
-const YT_REQ_URL = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${YT_CHANNEL_ID}&fields=items/statistics/subscriberCount&key=${YT_API_KEY}`
+const BILI_REQ_URL = `https://api.bilibili.com/x/relation/stat?vmid=${CHANNEL_CONFIGS.BILI_UID}`
+const YT_REQ_URL = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${CHANNEL_CONFIGS.YT_CHANNEL_ID}&fields=items/statistics/subscriberCount&key=${CHANNEL_CONFIGS.YT_API_KEY}`
 const BILI_STORE_KEY = 'LAST_BILI_FOLLOWER'
 const YT_STORE_KEY = 'LAST_YT_FOLLOWER'
 const BILI_ICON_BASE64 = 'AAABAAEAICAAAAEAIACoEAAAFgAAACgAAAAgAAAAQAAAAAEAIAAAAAAAABAAABMLAAATCwAAAAAAAAAAAAD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A1qEAANahAADWoQAG1qEAb9ahAMvWoQD01qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD01qEAy9ahAG/WoQAG1qEAANahAADWoQAA1qEAG9ahAM/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahANDWoQAb1qEAANahAAfWoQDQ1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahANHWoQAH1qEAbtahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAG7WoQDH1qEA/9ahAP/WoQD/1qEAtdahABjWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahABvWoQC11qEA/9ahAP/WoQD/1qEAx9ahAPnWoQD/1qEA/9ahAP/WoQAZ1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahABjWoQD/1qEA/9ahAP/WoQDz1qEA/9ahAP/WoQD/1qEA/9ahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEAANahAADWoQAA1qEAANahAErWoQDn1qEA5NahAErWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAErWoQDn1qEA5NahAErWoQAA1qEAANahAADWoQAA1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQAA1qEAANahAADWoQAA1qEA5tahAP/WoQD/1qEA59ahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEA5tahAP/WoQD/1qEA59ahAADWoQAA1qEAANahAADWoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAADWoQAA1qEAANahAADWoQD/1qEA/9ahAP/WoQD/1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQD/1qEA/9ahAP/WoQD/1qEAANahAADWoQAA1qEAANahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEAANahAADWoQAA1qEAANahAP/WoQD/1qEA/9ahAP/WoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAP/WoQD/1qEA/9ahAP/WoQAA1qEAANahAADWoQAA1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQAA1qEAANahAADWoQAA1qEA5tahAP/WoQD/1qEA5tahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEA5tahAP/WoQD/1qEA5tahAADWoQAA1qEAANahAADWoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAADWoQAA1qEAANahAADWoQBJ1qEA5tahAObWoQBJ1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQBJ1qEA5tahAObWoQBJ1qEAANahAADWoQAA1qEAANahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQD/1qEA/9ahAP/WoQD/1qEA+dahAP/WoQD/1qEA/9ahABnWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAGdahAP/WoQD/1qEA/9ahAPjWoQDH1qEA/9ahAP/WoQD/1qEAttahABnWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahABnWoQC21qEA/9ahAP/WoQD/1qEAx9ahAG3WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQBt1qEABtahAM/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA0NahAAfWoQAA1qEAG9ahAM/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAM/WoQAb1qEAANahAADWoQAA1qEABtahAG7WoQDH1qEA89ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA/9ahAP/WoQD/1qEA89ahAMfWoQBu1qEABtahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEADtahAMXWoQD/1qEA/9ahAP/WoQD/1qEAxdahAA/WoQAA1qEAANahAADWoQAA1qEADtahAMXWoQD/1qEA/9ahAP/WoQD/1qEAxdahAA/WoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAAbWoQDF1qEA/9ahAP/WoQD/1qEA/9ahAMXWoQAP1qEAANahAADWoQAA1qEAANahAADWoQAA1qEADtahAMXWoQD/1qEA/9ahAP/WoQD/1qEAxdahAAbWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAYtahAP/WoQD/1qEA/9ahAP/WoQDF1qEADtahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEADtahAMXWoQD/1qEA/9ahAP/WoQD/1qEAY9ahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQBf1qEA/9ahAP/WoQD/1qEAxdahAA7WoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEADtahAMXWoQD/1qEA/9ahAP/WoQBf1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAATWoQCg1qEA6tahAKjWoQAO1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEAANahAADWoQAA1qEADtahAKjWoQDr1qEAoNahAATWoQAA1qEAANahAADWoQAA1qEAAP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A///////////AAAADgAAAAQAAAAAAAAAAA///wAf//+AP///wD///8A////AP///wDw/w8A8P8PAPD/DwDw/w8A8P8PAPD/DwD///8A////AH///gA///wAAAAAAAAAAAgAAAAcAAAAP8A8A/+AfgH/gP8B/4H/gf+D/8H/////8='
 const YT_ICON_BASE64 = 'iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABJklEQVR4Ae2WpVaFQRSFcaeS4QVwIjTiLbgk7D1w9xeg4FQcOu6acHeXtNnDmsFd7in/WetLI+f7dbYDAFEsAUvgoeAQ5k5iSQFpIH1kkMyQFXJMTsgpwTuc6jlHes2M3qNP71mge7g76DLNg8gygZ1QckGmuSdZJ7Azq8RDCSQTCJGgBCoFBWqVQLegQJcSmPnWIr9oICMXcIr4C4FJJbD9rUX+NtzX+CIQlfVbgS0lcP4jAVNt/UCA7acCZ0oAPxfQdXMLFNUBvpHflvgbAVM7h0B6zrfeD3kB+Ucg/xLKf4biPyLJX3GH9GFUogRSBQUSTSBZEwskQpFs1USyl6E0gRSRJtJPhsnci1B69oVQeqzXzOo9+kmj3juBeDw2BkSxBCyBO+9s03HRLVCoAAAAAElFTkSuQmCC'
+
+const globalStats = {
+  youTubeEnabled: CHANNEL_CONFIGS.YT_API_KEY && CHANNEL_CONFIGS.YT_CHANNEL_ID,
+  biliEnabled: CHANNEL_CONFIGS.BILI_UID,
+}
 
 const widget = createWidget(await getData())
 
@@ -30,33 +37,35 @@ async function getData() {
     nickname: 'Unknow'
   }
   
-  const biliReq = new Request(BILI_REQ_URL)
-  const biliRes = await biliReq.loadJSON()
-  console.log(biliRes)
-
-  const youtubeReq = new Request(YT_REQ_URL)
-  youtubeReq.timeoutInterval = 3
-
-  let youtubeRes = null
-  try {
-    youtubeRes = JSON.parse(await youtubeReq.loadString())
-  } catch(e) {
-    console.log(e)
+  if (globalStats.biliEnabled) {
+    const biliReq = new Request(BILI_REQ_URL)
+    const biliRes = await biliReq.loadJSON()
+    const biliFollower = biliRes?.data?.follower
+    if (biliFollower) {
+      result.biliFollower = +biliFollower
+      Keychain.set(BILI_STORE_KEY, String(biliFollower))
+    } else {
+      result.biliFollower = Keychain.contains(BILI_STORE_KEY) ? +Keychain.get(BILI_STORE_KEY) : 0
+    }
   }
+
+  if (globalStats.youTubeEnabled) {
+    const youtubeReq = new Request(YT_REQ_URL)
+    youtubeReq.timeoutInterval = 3
   
-  const biliFollower = biliRes?.data?.follower
-  if (biliFollower) {
-    result.biliFollower = +biliFollower
-    Keychain.set(BILI_STORE_KEY, String(biliFollower))
-  } else {
-    result.biliFollower = Keychain.contains(BILI_STORE_KEY) ? +Keychain.get(BILI_STORE_KEY) : 0
-  }
-  const youtubeFollower = youtubeRes?.items?.[0]?.statistics?.subscriberCount
-  if (youtubeFollower) {
-    result.youtubeFollower = +youtubeFollower
-    Keychain.set(YT_STORE_KEY, String(youtubeFollower))
-  } else {
-    result.youtubeFollower = Keychain.contains(YT_STORE_KEY) ? +Keychain.get(YT_STORE_KEY) : 0
+    let youtubeRes = null
+    try {
+      youtubeRes = JSON.parse(await youtubeReq.loadString())
+    } catch(e) {
+      console.log(e)
+    }
+    const youtubeFollower = youtubeRes?.items?.[0]?.statistics?.subscriberCount
+    if (youtubeFollower) {
+      result.youtubeFollower = +youtubeFollower
+      Keychain.set(YT_STORE_KEY, String(youtubeFollower))
+    } else {
+      result.youtubeFollower = Keychain.contains(YT_STORE_KEY) ? +Keychain.get(YT_STORE_KEY) : 0
+    }
   }
   result.nickname = NAME
   result.avatar = await (new Request(CHANNEL_AVATAR_URL)).loadImage()
@@ -72,6 +81,8 @@ function formatFollower(num) {
 }
 
 function wrapChannelStack(widget, iconBase64, text) {
+  widget.addSpacer(5)
+
   const st = widget.addStack()
   const icon = st.addImage(Image.fromData(Data.fromBase64String(iconBase64)))
   icon.imageSize = new Size(16, 16)
@@ -119,20 +130,19 @@ function createWidget(options) {
   
   w.addSpacer(2)
   
-  const allFollowers = biliFollower + youtubeFollower
+  const allFollowers = (biliFollower || 0) + (youtubeFollower || 0)
   const fo = w.addText(formatFollower(allFollowers))
   fo.font = Font.heavySystemFont(45)
   fo.textColor = new Color('#FFF')
   fo.lineLimit = 1
   fo.minimumScaleFactor = 0.5
-  
-  w.addSpacer(5)
 
-  wrapChannelStack(w, BILI_ICON_BASE64, biliFollower)
+  if (globalStats.biliEnabled) {
+    wrapChannelStack(w, BILI_ICON_BASE64, biliFollower)
+  }
   
-  w.addSpacer(5)
-    
-  wrapChannelStack(w, YT_ICON_BASE64, youtubeFollower)
-
+  if (globalStats.youTubeEnabled) {
+    wrapChannelStack(w, YT_ICON_BASE64, youtubeFollower)
+  }
   return w
 }
